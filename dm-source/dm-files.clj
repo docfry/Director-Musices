dcf497308da913ff1379f3f125904f279ca4fc60
Anders Friberg <afriberg@kth.se>
2012-11-28 20:35:01 +0100

    director -> directory + each file on separate line

--- dm-source/dm-files.clj
+++ dm-source/dm-files.clj
@@ -3,14 +3,32 @@
 
 ["package-dm.lsp"
  {:directory "lib-core"
-  :files ["scoreobjects.lsp" "basicmacros.lsp" "infixmath.lsp" "musicio.lsp" "rulemacros.lsp" "parallelrulemacros.lsp"
-          "dm-objects.lsp" "initconvert.lsp" "save-pdm-score.lsp" "rule-groups.lsp" "syntobjects.lsp" "shapeobjects.lsp"
-          "midifileoutput.lsp" "midifileinput.lsp" "playlist.lsp" "midibasic-lw.lsp"]
+  :files ["scoreobjects.lsp" 
+          "basicmacros.lsp" 
+          "infixmath.lsp" 
+          "musicio.lsp" 
+          "rulemacros.lsp" 
+          "parallelrulemacros.lsp"
+          "dm-objects.lsp" 
+          "initconvert.lsp" 
+          "save-pdm-score.lsp" 
+          "rule-groups.lsp" 
+          "syntobjects.lsp" 
+          "shapeobjects.lsp"
+          "midifileoutput.lsp" 
+          "midifileinput.lsp" 
+          "playlist.lsp" 
+          "midibasic-lw.lsp"]
   }
  "init.lsp"
- {:director "rules"
-  :files ["frules1.lsp" "frules2.lsp" "Intonation.lsp"
-          "FinalRitard.lsp" "utilityrules.lsp" "Punctuation.lsp"
-          "phrasearch.lsp" "SyncOnMel.lsp"]
+ {:directory "rules"
+  :files ["frules1.lsp" 
+          "frules2.lsp" 
+          "Intonation.lsp"
+          "FinalRitard.lsp" 
+          "utilityrules.lsp" 
+          "Punctuation.lsp"
+          "phrasearch.lsp" 
+          "SyncOnMel.lsp"]
   }
  ]
