ff8bc1c6857ed37018d309b47b3b0537d4da5be4
Anders Friberg <afriberg@kth.se>
2012-11-28 20:29:58 +0100

    list of system files added

---
+++ dm-source/dm-files.clj
@@ -0,0 +1,16 @@
+;list of common lisp files
+;loaded by the clj system 
+
+["package-dm.lsp"
+ {:directory "lib-core"
+  :files ["scoreobjects.lsp" "basicmacros.lsp" "infixmath.lsp" "musicio.lsp" "rulemacros.lsp" "parallelrulemacros.lsp"
+          "dm-objects.lsp" "initconvert.lsp" "save-pdm-score.lsp" "rule-groups.lsp" "syntobjects.lsp" "shapeobjects.lsp"
+          "midifileoutput.lsp" "midifileinput.lsp" "playlist.lsp" "midibasic-lw.lsp"]
+  }
+ "init.lsp"
+ {:director "rules"
+  :files ["frules1.lsp" "frules2.lsp" "Intonation.lsp"
+          "FinalRitard.lsp" "utilityrules.lsp" "Punctuation.lsp"
+          "phrasearch.lsp" "SyncOnMel.lsp"]
+  }
+ ]
